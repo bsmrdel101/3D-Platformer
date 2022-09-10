173bbc8e2a6e79e293932a50e82ce28740c2e165
Bennett Smrdel <fireknights318@gmail.com>
2022-09-10 11:45:45 -0500

    Added hierarchy structure

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BEAN 
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+    }
+}
